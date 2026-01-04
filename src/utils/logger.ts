0b6e7f93aee0e2a2ec16db63333db812e0e07b2e
sithumini-silva <sithusilva94@gmail.com>
2026-01-04 20:12:01 +0530

    update meal view ui

---
+++ src/utils/logger.ts
@@ -0,0 +1,33 @@
+import winston from 'winston';
+
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+    }),
+    align(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    // Write all logs with importance level of `error` or less to `error.log`
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    // Write all logs with importance level of `info` or less to `combined.log`
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+export default logger;
